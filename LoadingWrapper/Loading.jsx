9139d3e5892f3df6bd1242a8ae9dfcfe8a22bd2f
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2022-09-28 15:39:52 +0530

    ring styling added

--- LoadingWrapper/Loading.jsx
+++ LoadingWrapper/Loading.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './Loading.scss';
-// import DualRing from './Loaders/DualRing';
-// import Ellipsis from './Loaders/Ellipsis';
-// import Ring from './Loaders/Ring';
-// import Ripple from './Loaders/Ripple';
-// import Spinner from './Loaders/Spinner';
+import DualRing from './Loaders/DualRing';
+import Ellipsis from './Loaders/Ellipsis';
+import Ripple from './Loaders/Ripple';
+import Spinner from './Loaders/Spinner';
+import Ring from './Loaders/Ring';
 
 export default function Loading({
   loading,
@@ -14,12 +14,11 @@ export default function Loading({
 }) {
   return loading ? (
     <div style={{ backgroundColor: overlayColor }} className="loading-overlay">
-      Test 123
-      {/* <Ripple color={spinnerColor} /> */}
-      {/* <Spinner color={spinnerColor} /> */}
-      {/* <DualRing color={spinnerColor} /> */}
-      {/* <Ellipsis color={spinnerColor} /> */}
-      {/* <Ring color={spinnerColor} /> */}
+      <Ripple color={spinnerColor} />
+      <Spinner color={spinnerColor} />
+      <DualRing color={spinnerColor} />
+      <Ellipsis color={spinnerColor} />
+      <Ring color={spinnerColor} />
     </div>
   ) : (
     <>{props.children}</>
